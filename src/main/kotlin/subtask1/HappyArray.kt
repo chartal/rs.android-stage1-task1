3324307b8e1e089a842eb66158d8b72598972690
Chartal <Chartal@mail.ru>
2020-03-28 11:13:29 +0300

    add: first task1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,57 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
-    fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+
+
+    fun converToHappy(sadArray: IntArray): IntArray {
+
+
+        fun isChanged(testArray: IntArray): Boolean {
+            var changed = false
+            for (i in 1..testArray.size - 2) {
+                if (testArray[i - 1] + testArray[i + 1] < testArray[i]) {
+                    changed = true
+                }
+            }
+            return changed
+        }
+
+
+        fun converToHappyInside(sadArray: IntArray): IntArray {
+
+            var list: ArrayList<Int> = ArrayList()
+            list.add(sadArray[0])
+            for (i in 1..sadArray.size - 2) {
+                if (sadArray[i - 1] + sadArray[i + 1] >= sadArray[i]) {
+
+                    list.add(sadArray[i])
+                }
+            }
+            list.add(sadArray[sadArray.size - 1])
+
+
+            //объявляем наш массив
+            var chartalArray = IntArray(list.size)
+            //заполняем остальные значения:
+            for (i in 0..chartalArray.size - 1) {
+                chartalArray[i] = list[i]
+            }
+            return chartalArray
+        }
+
+
+        var result = converToHappyInside(sadArray)
+        while (isChanged(result)) {
+            result = converToHappyInside(result)
+            println(result.joinToString("  "))
+        }
+
+        return result
     }
+
+
+
+
+
 }
