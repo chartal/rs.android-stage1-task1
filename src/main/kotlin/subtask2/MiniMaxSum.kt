a82b1dc04a7719c7c7000a0ff9df22bc18df27fc
Chartal <Chartal@mail.ru>
2020-03-28 11:27:26 +0300

    add: first task2

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,32 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+
+
+        var list: ArrayList<Int> = ArrayList()
+        for (i in 0..input.size - 1) {
+            list.add(input[i])
+        }
+
+
+        var sumList: ArrayList<Int> = ArrayList()
+        for (i in 0..list.size - 1) {
+            var testList: ArrayList<Int> = ArrayList()
+            for (k in 0..list.size - 1) {
+                testList.add(list[k])
+            }
+            testList.removeAt(i)
+            val summa = testList.sum()
+            sumList.add(summa)
+            println(testList.joinToString("  ") + "    cумма элементов = $summa")
+
+        }
+        println(sumList.joinToString("  "))
+        val minValue: Int = sumList.min()!!
+        val maxValue: Int = sumList.max()!!
+
+        return intArrayOf(minValue, maxValue)
+
     }
 }
