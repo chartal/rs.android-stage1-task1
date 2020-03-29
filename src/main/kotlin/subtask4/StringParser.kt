c175e1aaf1361c08e3606b6c48ac2723e396c358
Chartal <Chartal@mail.ru>
2020-03-29 21:44:06 +0300

    fix: error in 4 task

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -3,6 +3,7 @@ package subtask4
 class StringParser {
 
 
+
     fun getResult(inputString: String): Array<String> {
 
 
@@ -19,36 +20,142 @@ class StringParser {
 
         fun indication1(input: ArrayList<Char>): Boolean {
             var indication = false
+            var flag1 = false
+            var flag2 = false
+
             for (i in 0..input.size - 1) {
-                if (input[i] == '(' || input[i] == ')') {
-                    indication = true
+
+                if (input[i] == '(') {
+                    flag1 = true
                 }
+
+                if (input[i] == ')') {
+                    flag2 = true
+                }
+
             }
+            if (flag1 && flag2) {
+                indication = true
+            }
+
             return indication
         }
+
 
         fun indication2(input: ArrayList<Char>): Boolean {
             var indication = false
+            var flag1 = false
+            var flag2 = false
+
             for (i in 0..input.size - 1) {
-                if (input[i] == '[' || input[i] == ']') {
-                    indication = true
+
+                if (input[i] == '[') {
+                    flag1 = true
                 }
+
+                if (input[i] == ']') {
+                    flag2 = true
+                }
+
             }
+            if (flag1 && flag2) {
+                indication = true
+            }
+
             return indication
         }
+
 
         fun indication3(input: ArrayList<Char>): Boolean {
             var indication = false
+            var flag1 = false
+            var flag2 = false
+
             for (i in 0..input.size - 1) {
-                if (input[i] == '<' || input[i] == '>') {
-                    indication = true
+
+                if (input[i] == '<') {
+                    flag1 = true
                 }
+
+                if (input[i] == '>') {
+                    flag2 = true
+                }
+
             }
+            if (flag1 && flag2) {
+                indication = true
+            }
+
             return indication
         }
+        fun getFirstFornit(input: ArrayList<Char>): Int {
+
+            var flag1 = false
+            var flag2 = false
+            var flag3 = false
+
+            for (i in 0..input.size - 1) {
+
+                if ((input[i] == '(') && (flag2==false) && (flag3==false) && indication1(input)) {
+                    flag1 = true
+                }
+
+                if ((input[i] == '[') && (flag1==false) && (flag3==false) && indication2(input)) {
+                    flag2 = true
+                }
+
+                if ((input[i] == '<') && (flag1==false) && (flag2==false) && indication3(input) ) {
+                    flag3 = true
+                }
+            }
 
 
-        fun getResultInside1(input: ArrayList<Char>): ArrayList<Char> {
+            if (flag1) return 0
+            if (flag2) return 1
+            if (flag3) return 2
+
+            else return 5
+
+        }
+
+
+
+
+
+
+
+        fun indication(input: ArrayList<Char>): Boolean {
+
+            return (indication1(input) || indication2(input) || indication3(input))
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        fun getResultInside(input: ArrayList<Char>): ArrayList<Char> {
 
             val myString = input.joinToString("")
 
@@ -56,188 +163,102 @@ class StringParser {
             var b = 0
             var flag1 = false
             var flag2 = false
-            var flag3 = false
 
 
+            var arrayLeft: ArrayList<Char> = ArrayList()
+            var arrayCenter: ArrayList<Char> = ArrayList()
+            var arrayRight: ArrayList<Char> = ArrayList()
 
 
+            //определение порядка парсинга строки
+
+            var start: Char = '('
+            var end: Char = ')'
+
+            val fornit = getFirstFornit(input)
+            if (fornit == 0) {
+                start = '('
+                end = ')'
+            }
+
+            if (fornit == 1) {
+                start = '['
+                end = ']'
+            }
+
+            if (fornit == 2) {
+                start = '<'
+                end = '>'
+            }
 
 
+            //блок поиска части строки, заключенной в скобки
 
+            for (i in 0 until input.size) {
 
-            for (i in myString.indices) {
-
-                if (myString[i] == '(') {
-
-                    a = i
+                if ((input[i] == start) && !flag1) {
                     flag1 = true
-
-                }
-
-                if (myString[i] == ')' && flag1 == true) {
-
-                    b = i
-
-                }
-
-            }
-
-
-            val s: String = myString.subSequence(a + 1, b) as String
-            println(s)
-            list.add(s)
-            val result = myString.replaceRange(a, b + 1, "")
-
-
-            //преобразуем результ в массив символов
-
-            var resultArray: ArrayList<Char> = ArrayList()
-
-            for (char in result.toCharArray()) {
-                resultArray.add(char)
-            }
-
-            // println(resultArray.joinToString("  "))
-
-
-            return resultArray
-
-        }
-
-
-        fun getResultInside2(input: ArrayList<Char>): ArrayList<Char> {
-
-            val myString = input.joinToString("")
-
-            var a = 0
-            var b = 0
-            var flag1 = false
-            var flag2 = false
-            var flag3 = false
-
-
-
-            for (i in myString.indices) {
-
-                if (myString[i] == '[') {
-
                     a = i
-                    flag1 = true
 
                 }
 
-                if (myString[i] == ']' && flag1 == true) {
-
+                if ((input[i] == end) && flag1 && !flag2) {
                     b = i
+                    flag2 = true
+                }
+            }
 
+
+            //блок записи в массивы
+            for (i in 0 until input.size) {
+                if (i < a) {
+                    arrayLeft.add(input[i])
+                }
+                if ((a < i) && (i < b)) {
+                    arrayCenter.add(input[i])
+                }
+                if (i > b) {
+                    arrayRight.add(input[i])
                 }
 
             }
 
 
-            val s: String = myString.subSequence(a + 1, b) as String
-            println(s)
+
+
+            val s = arrayCenter.joinToString("")
             list.add(s)
-            val result = myString.replaceRange(a, b + 1, "")
 
 
-            //преобразуем результ в массив символов
-
-            var resultArray: ArrayList<Char> = ArrayList()
-
-            for (char in result.toCharArray()) {
-                resultArray.add(char)
-            }
-
-            // println(resultArray.joinToString("  "))
-
-
-            return resultArray
-
-
-        }
-
-
-        fun getResultInside3(input: ArrayList<Char>): ArrayList<Char> {
-
-            val myString = input.joinToString("")
-
-            var a = 0
-            var b = 0
-            var flag1 = false
-            var flag2 = false
-            var flag3 = false
-
-
-
-            for (i in myString.indices) {
-
-                if (myString[i] == '<') {
-
-                    a = i
-                    flag1 = true
-
-                }
-
-                if (myString[i] == '>' && flag1 == true) {
-
-                    b = i
-
-                }
+            while (indication(arrayCenter)) {
+                arrayCenter = getResultInside(arrayCenter)
 
             }
 
 
-            val s: String = myString.subSequence(a + 1, b) as String
-            println(s)
-            list.add(s)
-            val result = myString.replaceRange(a, b + 1, "")
 
 
-            //преобразуем результ в массив символов
-
-            var resultArray: ArrayList<Char> = ArrayList()
-
-            for (char in result.toCharArray()) {
-                resultArray.add(char)
-            }
-
-            // println(resultArray.joinToString("  "))
-
-
-            return resultArray
-
+            val result = (arrayLeft + arrayRight) as ArrayList<Char>
+            return result
 
         }
 
 
-        var testArray1 = getResultInside1(input)
-        //println(testArray.joinToString(""))
-        while (indication1(testArray1)) {
-            testArray1 = getResultInside1(testArray1)
-
-        }
-
-        var testArray2 = getResultInside2(input)
-        //println(testArray.joinToString(""))
-        while (indication2(testArray2)) {
-            testArray2 = getResultInside2(testArray2)
-
-        }
+        //поиск строк внутри круглых скобок
 
 
-        var testArray3 = getResultInside3(input)
-        //println(testArray.joinToString(""))
-        while (indication3(testArray3)) {
-            testArray3 = getResultInside3(testArray3)
+        while (indication(input)) {
+            input = getResultInside(input)
 
         }
 
 
         var exitArray: Array<String> = list.toTypedArray()
-        exitArray.reverse()
-        println("Вот мой массив:" + exitArray.joinToString (" "))
+        //exitArray.reverse()
 
+
+        println("Вот исходная строка: " + inputString)
+        println("Вот мой массив: " + exitArray.joinToString("  "))
 
         return exitArray
 
@@ -245,7 +266,5 @@ class StringParser {
     }
 
 
-
-
-
 }
+
